eaf68065576c3a2eeb4b6378e9056470d6dc2f9f
Sandeep <stbt4@mst.edu>
2018-04-04 23:30:48 -0700

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -1,25 +1,12 @@
-/**
- * Created by sandeeptc on 6/7/17.
- */
-
 function anagrams(a,b) {
-    var word = a;
-    var wordSplit = word.toLowerCase().split('');
-    var wordSorted = wordSplit.sort().join("");
-
-    var word2 = b;
-    var word2Split = word2.toLowerCase().split('');
-    var word2Sorted = word2Split.sort().join("");
-
-    // console.log(wordSorted);
-    // console.log(word2Sorted);
-    if (wordSorted.length===word2Sorted.length)
+    var word1 = a.toLowerCase().split('').sort().join('');
+    var word2 = b.toLowerCase().split('').sort().join('');
+    if (word1.length===word2.length)
     {
-        if (wordSorted === word2Sorted) {
+        if (word1 === word2) {
             console.log('Anagrams!');
         }
     }
-
     else {
         console.log('Not Anagrams!');
     }

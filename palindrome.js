d1c8c0f3a095baa6db2a15093dd02994b280c3ad
Sandeep <stbt4@mst.edu>
2018-04-04 23:44:26 -0700

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,15 +1,9 @@
 /**
  * Created by sandeeptc on 6/7/17.
  */
-
-function ispalindrome(x) {
-
-    var a = x;
-    var b = a.split("");
-    var c = b.reverse().join("");
-    console.log(c);
-
-    if (a === c) {
+function ispalindrome(str) {
+    var str1=str.split('').reverse().join('');
+    if (str1 === str) {
         console.log("The given string is palindrome!!!");
     }
     else {
